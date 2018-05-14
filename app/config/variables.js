8c32f42305c74e55211412178f62e5b5b9750870
havard <havardnoren@gmail.com>
2018-05-14 10:49:06 +0200

    fix app not loading on ios

--- app/config/variables.js
+++ app/config/variables.js
@@ -2,6 +2,7 @@ import {Dimensions, Platform} from "react-native";
 
 // Copied from native base since they don't expose the variables through the library
 export const deviceWidth = Dimensions.get("window").width;
+export const deviceHeight = Dimensions.get("window").height;
 
 const platform = Platform.OS;
 const isIphoneX =

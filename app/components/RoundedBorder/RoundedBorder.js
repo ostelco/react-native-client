986c00efb2532021b818c07bd111ceaefffe7bf6
havard <havardnoren@gmail.com>
2018-05-25 16:13:33 +0200

    feat(RoundedBorder): make background color configurable

--- app/components/RoundedBorder/RoundedBorder.js
+++ app/components/RoundedBorder/RoundedBorder.js
@@ -1,11 +1,24 @@
 import React from "react";
 import styles from "./styles";
 import {View} from "native-base";
+import PropTypes from 'prop-types';
 
-const RoundedBorder = () => (
-  <View style={styles.container}>
-    <View style={styles.content}></View>
-  </View>
-);
+const RoundedBorder = props => {
+  const { color } = props;
+  let extraStyles = {};
+  if (color) {
+    extraStyles.backgroundColor = color;
+  }
+  return (
+    <View style={styles.container}>
+      <View style={[styles.content, extraStyles]}></View>
+    </View>
+  );
+};
+
+RoundedBorder.propTypes = {
+  color: PropTypes.string,
+};
+
 
 export default RoundedBorder;

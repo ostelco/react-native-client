99247fed207dab5939bb056fc4d0455ab3991b57
havard <havardnoren@gmail.com>
2018-05-25 16:13:45 +0200

    refactor(Home): move inline styles to stylesheet in separate file

--- app/containers/Home/Home.js
+++ app/containers/Home/Home.js
@@ -10,60 +10,34 @@ import {
   List,
   ListItem,
   View,
-  Right, Spinner
+  Right,
+  Spinner,
+  Left
 } from "native-base";
 import PropTypes from 'prop-types';
 import {textStyles} from "../../config/fonts";
 import {colors} from "../../config/colors";
-import {StyleSheet, TouchableHighlight } from "react-native";
-import { toolbarHeight, deviceWidth } from '../../config/variables';
-
-const styles = StyleSheet.create({
-  content: {
-    flex: 1,
-    // padding: 25,
-    // paddingTop: 30,
-    // paddingRight: 30,
-    backgroundColor: null,
-    // marginTop: 30
-  },
-  header: {
-    backgroundColor: colors.rosa,
-
-  },
-  roundedBox: {
-    // Make perfect circle
-    width: deviceWidth,
-    borderBottomRightRadius: deviceWidth / 2,
-    borderBottomLeftRadius: deviceWidth / 2,
-    height: deviceWidth,
-    backgroundColor: colors.rosa,
-    transform: [
-      // Create oval by scaling X to get correct angle on border
-      {scaleX: 1.8},
-      // Expose bottom part of oval in the margin of the content between the header and content
-    ],
-    // Make so shape does not occupy any space on the screen, use translate to position it where it's wanted
-    marginTop: -deviceWidth + 35,
-  }
-});
+import { TouchableHighlight } from "react-native";
+import styles from './styles';
+import {RoundedBorder} from "../../components";
 
 const Home = props => {
-  const { style, showMenu, showPayment, dataLeft, defaultOffer, specialOffer } = props;
+  const { showMenu, showPayment, dataLeft, defaultOffer, specialOffer } = props;
   return (
-    <Container style={{ position: 'relative', backgroundColor: colors.whiteTwo }}>
-      <Header noShadow androidStatusBarColor={'rgba(0,0,0,0.5)'} style={[style.header, { position: 'relative', justifyContent: 'space-evenly' }]}>
-        <Body style={{ alignItems: 'center', flex: 1 }}>
-          <Title>
-            <Text style={textStyles.textStyle11}>pi</Text>
+    <Container style={styles.container}>
+      <Header noShadow androidStatusBarColor={'rgba(0,0,0,0.5)'} style={styles.header}>
+        <Left></Left>
+        <Body>
+          <Title style={textStyles.textStyle11}>
+            pi
           </Title>
         </Body>
-        <Right style={{ flex: -1 }}>
-          <Icon name="menu" style={{ color: colors.white }} onPress={showMenu} />
+        <Right>
+          <Icon name="menu" style={styles.headerRightButton} onPress={showMenu} />
         </Right>
       </Header>
-      <Content contentContainerStyle={style.content} bounces={false}>
-        <List style={{ backgroundColor: colors.rosa }}>
+      <Content contentContainerStyle={styles.content} bounces={false}>
+        <List style={styles.topContentContainer}>
           <ListItem noBorder>
             { dataLeft ? (
               <Body>
@@ -75,58 +49,28 @@ const Home = props => {
               ) : <Body><Spinner color="white" /></Body>}
           </ListItem>
           <ListItem noBorder onPress={() => showPayment(defaultOffer)}>
-            <Body style={{ justifyContent: 'center', flex: 1, alignItems: 'center' }}>
-              <View style={{
-                width: 220,
-                height: 60,
-                borderRadius: 5,
-                paddingHorizontal: 40,
-                backgroundColor: colors.white,
-                flexDirection: 'row',
-                justifyContent: 'space-evenly',
-                alignItems: 'center'
-              }}>
-                  <Text style={[textStyles.textStyle14, { textAlign: 'center' }]}>{defaultOffer ? defaultOffer.sku : ''}</Text>
-                  <Text style={[textStyles.textStyle15, { textAlign: 'center' }]}>40 NOK</Text>
+            <Body style={styles.staticOfferContainer}>
+              <View style={styles.staticOfferButtonContainer}>
+                  <Text style={[textStyles.textStyle14, styles.offerButton]}>{defaultOffer ? defaultOffer.sku : ''}</Text>
+                  <Text style={[textStyles.textStyle15, styles.offerButton]}>40 NOK</Text>
               </View>
             </Body>
           </ListItem>
         </List>
-        <View style={{ height: 35, overflow: 'hidden', width: '100%', zIndex: -1 }}>
-          <View style={style.roundedBox}>
-          </View>
-        </View>
+        <RoundedBorder color={colors.rosa} />
         { specialOffer ? (
-          <View style={{
-            padding: 30
-          }}>
-            <View style={{
-              width: '100%',
-              height: 225,
-              borderRadius: 5,
-              backgroundColor: colors.white,
-              paddingHorizontal: 30,
-              justifyContent: 'space-evenly',
-              alignItems: 'center'
-            }}>
+          <View style={styles.specialOfferContainer}>
+            <View style={styles.specialOffer}>
               <Text style={textStyles.textStyle8}>
                 Monday Special!
               </Text>
               <TouchableHighlight onPress={() => showPayment(specialOffer)}>
-                <View style={{
-                  width: 220,
-                  height: 60,
-                  borderRadius: 5,
-                  backgroundColor: colors.rosa,
-                  paddingHorizontal: 40,
-                  flexDirection: 'row',
-                  justifyContent: 'center'
-                }}>
-                  <View style={{ flex: 1, justifyContent: 'center' }}>
-                    <Text style={[textStyles.textStyle16, { textAlign: 'center' }]}>{specialOffer ? specialOffer.sku : ''}</Text>
+                <View style={styles.specialOfferButtonContainer}>
+                  <View style={styles.specialOfferButtonTextContainer}>
+                    <Text style={[textStyles.textStyle16, styles.offerButton]}>{specialOffer ? specialOffer.sku : ''}</Text>
                   </View>
-                  <View style={{ flex: 1, justifyContent: 'center' }}>
-                    <Text style={[textStyles.textStyle17, { textAlign: 'center' }]}>{specialOffer.price.amount} {specialOffer.price.currency}</Text>
+                  <View style={styles.specialOfferButtonTextContainer}>
+                    <Text style={[textStyles.textStyle17, styles.offerButton]}>{specialOffer.price.amount} {specialOffer.price.currency}</Text>
                   </View>
                 </View>
               </TouchableHighlight>
@@ -135,7 +79,18 @@ const Home = props => {
               </Text>
             </View>
           </View>
-        ) : null}
+        ) : (
+          <View style={styles.specialOfferContainer}>
+            <View style={styles.specialOffer}>
+              <Text style={textStyles.textStyle8}>
+                <Icon name="basket" style={styles.emptyOfferIcon} />
+              </Text>
+              <Text style={textStyles.textStyle18}>
+                Once we’ve learned a little bit more about you, you will find personalized offers here!
+              </Text>
+            </View>
+          </View>
+        )}
       </Content>
     </Container>
   )
@@ -148,10 +103,6 @@ Home.propTypes = {
   dataLeft: PropTypes.string,
   defaultOffer: PropTypes.object,
   specialOffer: PropTypes.object
-};
-
-Home.defaultProps = {
-  style: styles,
 };
 
 export default Home;

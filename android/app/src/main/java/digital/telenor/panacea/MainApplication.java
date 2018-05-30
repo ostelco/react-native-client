93909048f969b294b73393429f473a1ee1f5724e
havard <havardnoren@gmail.com>
2018-05-30 10:12:10 +0200

    feat: add crashlytics for android

--- android/app/src/main/java/digital/telenor/panacea/MainApplication.java
+++ android/app/src/main/java/digital/telenor/panacea/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
+import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,11 +26,11 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNFirebasePackage(),
-            new A0Auth0Package(),
-            new RNFirebaseAnalyticsPackage()
-
+        new MainReactPackage(),
+        new RNFirebasePackage(),
+        new A0Auth0Package(),
+        new RNFirebaseAnalyticsPackage(),
+        new RNFirebaseCrashlyticsPackage()
       );
     }
 

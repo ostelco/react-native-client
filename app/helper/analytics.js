8193941e68a3b04e606493e19f914df616a84c49
havard <havardnoren@gmail.com>
2018-06-05 15:53:22 +0200

    feat: add global object to set global params when sending analytic events

--- app/helper/analytics.js
+++ app/helper/analytics.js
@@ -2,16 +2,22 @@ import firebase from 'react-native-firebase'
 
 const analytics = firebase.analytics();
 
+export const globalParams = {};
+
+const _logEvent = (name, params) => {
+  analytics.logEvent(name, { ...params, ...globalParams });
+}
+
 export const setCurrentScreen = (screenName) => {
   analytics.setCurrentScreen(screenName, screenName);
 };
 
 export const logGDPRPermissionEvent = params => {
-  analytics.logEvent('gdpr_permission', params);
+  _logEvent('gdpr_permission', params);
 };
 
 export const logAddToCartEvent = product => {
-  analytics.logEvent('add_to_cart', {
+  _logEvent('add_to_cart', {
     quantity: 1,
     price: (product.price.amount / 100.0).toFixed(2),
     value: (product.price.amount / 100.0).toFixed(2),
@@ -23,7 +29,7 @@ export const logAddToCartEvent = product => {
 };
 
 export const logECommercePurchaseEvent = product => {
-  analytics.logEvent('ecommerce_purchase', {
+  _logEvent('ecommerce_purchase', {
     quantity: 1,
     price: (product.price.amount / 100.0).toFixed(2),
     value: (product.price.amount / 100.0).toFixed(2),
@@ -35,13 +41,13 @@ export const logECommercePurchaseEvent = product => {
 };
 
 export const logLoginEvent = () => {
-  analytics.logEvent('login', {
+  _logEvent('login', {
     sign_up_method: 'google'
   })
 };
 
 export const logSignUpEvent = () => {
-  analytics.logEvent('signup')
+  _logEvent('signup')
 }
 
 export default {

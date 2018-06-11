801f603f72595d6b67809325ae5365e0ae4a8164
havard <havardnoren@gmail.com>
2018-06-11 15:15:56 +0200

    refactor: recompose SpecialOffer and Offer containers

--- app/containers/Home/OfferContainer.js
+++ app/containers/Home/OfferContainer.js
@@ -9,7 +9,7 @@ import screens from "../../helper/screens";
 import { withActions, withDefaultProduct, getProductSKU, getProductPrice, getProductCurrency } from '../../helper/enhancers';
 import {getDefaultProductBySKU} from "../../helper/graphql";
 import {formatPriceByPriceLabel} from "../../helper/price";
-import {Query} from "react-apollo";
+import {Query, withApollo, graphql} from "react-apollo";
 import {Body, Text} from "native-base";
 
 const OfferContainer = compose(
@@ -30,26 +30,27 @@ const OfferContainer = compose(
       logAddToCartEvent(product);
       navigation.navigate(screens.Payment);
     }
-  }))
-)(props => { // TODO: Create HOC of below, synchronize with some issue in SpecialOfferContainer
-  const { sku, handlePress, price, currency } = props;
-  return (
-    <Query query={getDefaultProductBySKU} variables={{ sku }}>
-      {compose(
-        branch(({ loading }) => loading, renderNothing),
-        branch(({ error }) => error, renderComponent(({ error }) => <Body style={{ alignItems: 'center' }}><Text>ERROR! {error}</Text></Body>)),
-        mapProps(({ data }) => {
-          const { productLabel } = data.DefaultProduct.translations[0];
-          const { priceLabel } = data.DefaultProduct;
-          return ({
-            productLabel,
-            priceLabel: formatPriceByPriceLabel(priceLabel, price, currency),
-            handlePress,
-          })
-        })
-      )(Offer)}
-    </Query>
-  )
-});
+  })),
+  withApollo,
+  graphql(getDefaultProductBySKU, {
+    options: ({ sku }) => {
+      return ({
+        variables: {
+          sku
+        }
+      })
+    }
+  }),
+  branch(({ data }) => data.loading, renderNothing),
+  branch(({ data }) => data.error, renderComponent(({ error }) => <Body style={{ alignItems: 'center' }}><Text>ERROR! {error}</Text></Body>)),
+  mapProps(({ data, price, currency }) => {
+    const { productLabel } = data.DefaultProduct.translations[0];
+    const { priceLabel } = data.DefaultProduct;
+    return ({
+      productLabel,
+      priceLabel: formatPriceByPriceLabel(priceLabel, price, currency),
+    })
+  })
+)(Offer);
 
 export default OfferContainer;
--- app/containers/Home/SpecialOfferContainer.js
+++ app/containers/Home/SpecialOfferContainer.js
@@ -9,7 +9,7 @@ import screens from "../../helper/screens";
 import { EmptySpecialOffer } from './SpecialOffer';
 import {formatPriceByPriceLabel} from "../../helper/price";
 import {getOfferProductBySKU} from "../../helper/graphql";
-import {Query} from "react-apollo";
+import {Query, withApollo, graphql} from "react-apollo";
 import {Body, Text} from "native-base";
 
 export default compose(
@@ -28,26 +28,27 @@ export default compose(
       logAddToCartEvent(product);
       navigation.navigate(screens.Payment)
     }
-  }))
-)(props => { // TODO: Create HOC of below, synchronize with some issue in OfferContainer
-  const { sku, handlePress, price, currency } = props;
-  return (
-    <Query query={getOfferProductBySKU} variables={{ sku }}>
-      {compose(
-        branch(({ loading }) => loading, renderComponent(EmptySpecialOffer)),
-        branch(({ error }) => error, renderComponent(({ error }) => <Body style={{ alignItems: 'center' }}><Text>ERROR! {error}</Text></Body>)),
-        mapProps(({ data }) => {
-          const { offerLabel, productLabel, description } = data.OfferProduct.translations[0];
-          const { priceLabel } = data.OfferProduct;
-          return ({
-            offerLabel,
-            productLabel,
-            priceLabel: formatPriceByPriceLabel(priceLabel, price, currency),
-            productDescription: description,
-            handlePress,
-          })
-        })
-      )(SpecialOffer)}
-    </Query>
-  )
-});
+  })),
+  withApollo,
+  graphql(getOfferProductBySKU, {
+    options: ({ sku }) => {
+      return ({
+        variables: {
+          sku
+        }
+      })
+    }
+  }),
+  branch(({ data: { loading } }) => loading, renderComponent(EmptySpecialOffer)),
+  branch(({ data: { error } }) => error, renderComponent(({ error }) => <Body style={{ alignItems: 'center' }}><Text>ERROR! {error}</Text></Body>)),
+  mapProps(({ data, price, currency }) => {
+    const { offerLabel, productLabel, description } = data.OfferProduct.translations[0];
+    const { priceLabel } = data.OfferProduct;
+    return ({
+      offerLabel,
+      productLabel,
+      priceLabel: formatPriceByPriceLabel(priceLabel, price, currency),
+      productDescription: description,
+    })
+  })
+)(SpecialOffer);

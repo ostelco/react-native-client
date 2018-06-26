4b5ad24e63c4cdfca33aa07422db67d4835890cd
havard <havardnoren@gmail.com>
2018-06-26 02:36:29 +0200

    feat: cache subscription to keep data left an reenter

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -8,7 +8,7 @@ import rootReducer from '../reducers'
 const persistConfig = {
   key: 'root',
   storage: storage,
-  whitelist: ['pseudonyms', 'auth', 'profile', 'products']
+  whitelist: ['pseudonyms', 'auth', 'profile', 'products', 'subscription']
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
